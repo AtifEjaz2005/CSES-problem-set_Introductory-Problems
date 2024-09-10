83d4133c282b1128544fe2cc60b6cfc62de6f999
Atif Ejaz <atifejaz862@gmail.com>
2024-09-11 01:53:35 +0500

    Weird Algorithm.cpp

--- Weird Algorithm.cpp
+++ Weird Algorithm.cpp
@@ -7,15 +7,15 @@ int main (){
 	cout<<"Enter a number : ";
 	cin>>number;
 	
-	cout<<number<<"---->";
+	cout<<number;
 	
 	while (number != 1 ){	
 		if (number % 2 == 0 ){
 			number = number / 2 ;
-			cout<<number<<"---->";
+			cout<<" ----> "number;
 		}else{
 			number =  ( number * 3 ) + 1 ;
-			cout<<number<<" ----> ";
+			cout<<" ----> "number;
 		}
 	}
 	return 0 ;
